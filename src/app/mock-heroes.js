816b68f4e82d6176b4715952dad2f760ed323bb9
Louinis Boudaa <lboudaa@student.42.fr>
2017-04-17 22:38:08 +0200

    up till the route

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Luffy' },
+    { id: 12, name: 'Zoro' },
+    { id: 13, name: 'Sanji' },
+    { id: 14, name: 'Nami' },
+    { id: 15, name: 'Chopper' },
+    { id: 16, name: 'Robin' },
+    { id: 17, name: 'Sunny' },
+    { id: 18, name: 'Franky' },
+    { id: 19, name: 'Ussop' },
+    { id: 20, name: 'Brook' }
+];
+//# sourceMappingURL=mock-heroes.js.map
